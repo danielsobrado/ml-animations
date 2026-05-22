2dc55b870cc7b85c02648485eb16ad712a8c7415
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-05-22 15:41:54 +0400

    Deploy GitHub Pages

--- assets/mouse-pointer-2-B64an9C2.js
+++
@@ -1,6 +0,0 @@
-import{Y as e}from"./index-B3cHPdpd.js";/**
- * @license lucide-react v0.294.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=e("MousePointer2",[["path",{d:"m4 4 7.07 17 2.51-7.39L21 11.07z",key:"1vqm48"}]]);export{t as M};
